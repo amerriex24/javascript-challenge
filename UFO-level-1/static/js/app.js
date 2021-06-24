5ea0387b0ab5fc0cd9564dacf7197b5e9c49a8cb
Alex Merriex <alexrmerriex@gmail.com>
2021-06-23 23:09:52 -0600

    Code cleaning

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,6 +4,18 @@ var tableData = data;
 //Reference the table body 
 var tbody = d3.select("tbody");
 
-
-
 // YOUR CODE HERE!
+var button=d3.select('#filter-btn');
+var inpt01=d3.select('#datetime');
+var inpt02=d3.select('#city');
+var inpt03=d3.select('#state')
+var cols=['datetime','city','state','country','shape','durationMinutes','comments']
+var resetbtn=d3.select('#reset-btn');
+
+function tblOn(dataObj){
+    dataObj.forEach(line => {
+    let row=tbody.append('tr');
+    cols.forEach(col => row.append('td').text(line[col]))
+    });
+
+}
